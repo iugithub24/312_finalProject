b5b30b3446d7a5f30fc280e6991abf9f8cefa37d
Areeb Shaikh <arshaikh@iu.edu>
2024-04-25 22:26:08 -0400

    fixed questions

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let questionSet = [
     { question: "How many CIT concentrations are there?", answer: "4" },
     { question: "What is the highest paying CIT concentration?", answer: "Web Development" },
     { question: "Which of the following is not a CIT career?", answer: "Mechanical Administrator" },
-    { question: "CIT is a 6 year degree?", answer: "False" },
+    { question: "True or False: CIT is a 6 year degree?", answer: "False" },
     { question: "What is the percentage outlook of technical writers between 2022 & 2032?", answer: "10%" },
     { question: "What is the name of a legally/ethically employed hacker?", answer: "White-Hat Hacker" },
-    { question: "CIT does not have a masters program?", answer: "False" },
+    { question: "True or False: CIT does not have a masters program?", answer: "False" },
     { question: "Which of the following is not a CIT career?", answer: "Mechanical Administrator" },
     { question: "Are Web/Software Developers able to work remote?", answer: "Yes" },
     { question: "What is the average salary of a Game Developer?", answer: "$70,000/year" }
